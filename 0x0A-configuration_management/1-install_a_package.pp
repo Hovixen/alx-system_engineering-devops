94a1ac126cc9e59f1c79f4953629ddc486e5b446
Hovixen <orevaerijo@gmail.com>
2024-01-20 01:03:22 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 # manifest installs flask from pip3
 
-exec {'pip3':
-command => '/usr/bin/pip3'
+package {'python3-pip':
+ensure => 'installed'
 }
 
 package {'flask':
-ensure  => '2.1.0',
-require => Exec['pip3']
+ensure   => '2.1.0',
+provider => 'pip3'
+require  => Package['python3-pip']
 }
