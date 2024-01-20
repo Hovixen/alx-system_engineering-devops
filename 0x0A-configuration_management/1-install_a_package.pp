81349be5d3ef443941296ea420dc7dc7eef04aed
Hovixen <orevaerijo@gmail.com>
2024-01-20 18:41:28 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -9,3 +9,9 @@ package { 'flask':
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
+
+package { 'werkzeug':
+  ensure   =>  '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
