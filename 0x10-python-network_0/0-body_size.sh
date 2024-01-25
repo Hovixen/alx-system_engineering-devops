f3376a6849f4d580d3ffd8ce0b88d2781709a105
Hovixen <orevaerijo@gmail.com>
2024-01-25 16:21:30 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Script takes URL and sends request to URL and displays the size body of response
 curl -sI $1 | grep -i content-length | awk '{print $2}' | tr -d '\r'
